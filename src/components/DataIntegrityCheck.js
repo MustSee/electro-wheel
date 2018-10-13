1839b8434a73938b76a9ca98d27b596b60adb58b
Thomas <thomas.sypniewski@gmail.com>
2018-10-13 21:47:53 +0200

    add button to add possibility to remove IDs if an error was made

--- src/components/DataIntegrityCheck.js
+++ src/components/DataIntegrityCheck.js
@@ -10,15 +10,14 @@ class DataIntegrityCheck extends Component {
     }
   }
 
-  handleClick = () => {
-    console.log('ischecking', this.state.isChecking);
+  handlePush = () => {
     this.setState({isChecking: true});
     const { musicGenre, artistName, piece, videos } = this.props.data;
     const { title } = piece;
     const type = piece.type + "s";
     const ref = firebase
       .database()
-      .ref(`music/${musicGenre}/${artistName}/${type}/${title}`);
+      .ref(`music/${musicGenre}/${artistName}/${type}/${title}/${videos.type}`);
 
     let payload;
     if (videos.type === "video") {
@@ -46,7 +45,6 @@ class DataIntegrityCheck extends Component {
         if (snapshot.exists()) {
           snapshot.forEach(data => {
             const value = data.val();
-            console.log('value.playlistId', value.playlistId, 'payload.playlistId', payload.playlistId);
             if (value.playlistId !== payload.playlistId) {
               ref.push(payload);
             }
@@ -60,15 +58,57 @@ class DataIntegrityCheck extends Component {
     }
   };
 
+  handleRemove = () => {
+    this.setState({isChecking: true});
+    const { musicGenre, artistName, piece, videos } = this.props.data;
+    const { title } = piece;
+    const type = piece.type + "s";
+    const ref = firebase
+      .database()
+      .ref(`music/${musicGenre}/${artistName}/${type}/${title}/${videos.type}`);
+
+    let payload;
+    if (videos.type === "video") {
+      payload = {
+        videoId: videos.videos[0].videoId,
+        title: videos.videos[0].title
+      };
+      ref.orderByChild('videoId').equalTo(payload.videoId).once("value", snapshot => {
+        if (snapshot.exists()) {
+          snapshot.forEach((data) => {
+              ref.child(data.key).remove();
+          });
+        }
+        this.setState({isChecking: false});
+      });
+    } else {
+      payload = { playlistId: videos.playlistId, videos: videos.videos };
+      ref.orderByChild('playlistId').equalTo(payload.playlistId).once("value", snapshot => {
+        if (snapshot.exists()) {
+          snapshot.forEach((data) => {
+              ref.child(data.key).remove();
+          });
+        }
+        this.setState({isChecking: false});
+      });
+    }
+  };
+
   render() {
     const isChecking = this.state.isChecking;
-    console.log(isChecking);
     return (
-      <div className="dataIntegrityButton">
-        <Button variant="contained" size="medium" onClick={!isChecking ? this.handleClick : null}>
-          Get IDs
-        </Button>
-      </div>
+      <React.Fragment>
+        <div className="dataIntegrityRemoveButton">
+          <Button variant="contained" size="medium" onClick={!isChecking ? this.handleRemove : null}>
+            Suppress IDs
+          </Button>
+        </div>
+        <div className="dataIntegrityPushButton">
+          <Button variant="contained" size="medium" onClick={!isChecking ? this.handlePush : null}>
+            Get IDs
+          </Button>
+        </div>
+      </React.Fragment>
     );
   }
 }
