0cd1a2d8290351ea9577f818a6ebdd2e3c71a922
Thomas <thomas.sypniewski@gmail.com>
2018-10-28 15:28:02 +0100

    working on refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import ReactTestUtils from 'react-dom/test-utils';
+
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
