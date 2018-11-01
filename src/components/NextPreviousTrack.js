d743353d9bc9e60e45c7d933b57b3620b5abfaad
Thomas <thomas.sypniewski@gmail.com>
2018-11-01 14:07:52 +0100

    make next previous track component a functionnal component

--- src/components/NextPreviousTrack.js
+++ src/components/NextPreviousTrack.js
@@ -1,43 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import { Chip, Icon, IconButton, Typography } from "@material-ui/core";
 
-class NextPreviousTrack extends Component {
-  clickPreviousTrack = () => {
-    this.props.previousTrack({ value: -1 });
-  };
+const label = (trackItemNumber, tracksNumber) => {
+  return (
+    <Typography variant="body1">
+      {trackItemNumber + 1} / {tracksNumber}
+    </Typography>
+  );
+};
 
-  clickNextTrack = () => {
-    this.props.nextTrack({ value: 1 });
-  };
+function NextPreviousTrack(props) {
+    const { trackItemNumber, tracksNumber } = props;
 
-  label = () => {
-    return (
-      <Typography variant="body1">
-        {this.props.trackItemNumber + 1} / {this.props.tracksNumber}
-      </Typography>
-    );
-  };
-
-  render() {
-    const { trackItemNumber, tracksNumber } = this.props;
     return (
       <div className="video_tracks_wrapper">
         <IconButton
-          onClick={this.clickPreviousTrack}
+          onClick={() => props.previousTrack({ value: -1 })}
           disabled={trackItemNumber === 0}
         >
           <Icon>skip_previous_button</Icon>
         </IconButton>
-        <Chip label={this.label()} variant="outlined" />
+        <Chip label={label(trackItemNumber, tracksNumber)} variant="outlined" />
         <IconButton
-          onClick={this.clickNextTrack}
+          onClick={() => props.nextTrack({ value: 1 })}
           disabled={trackItemNumber === tracksNumber - 1}
         >
           <Icon>skip_next_button</Icon>
         </IconButton>
       </div>
     );
-  }
 }
 
 export default NextPreviousTrack;
