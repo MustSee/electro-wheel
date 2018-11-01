f69ae43ffb49bce330d3427474169ea84bd722cc
Thomas <thomas.sypniewski@gmail.com>
2018-11-01 13:50:59 +0100

    change main action button component to a functionnal component

--- src/components/MainActionButton.js
+++ src/components/MainActionButton.js
@@ -1,33 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button } from "@material-ui/core";
 
-const styles = {
-  mainActionButton: {
-    height: "40px",
-    backgroundColor: "#ff9100"
-  }
-};
-
-class MainActionButton extends Component {
-  handleClickOnSearchButton = () => {
-    this.props.handleMainSearch();
+function MainActionButton(props) {
+  const styles = {
+    mainActionButton: {
+      height: "40px",
+      backgroundColor: "#ff9100"
+    }
   };
 
-  render() {
-    return (
-      <div className="musicInfo_mainActionButton_wrapper">
-        <Button
-          onClick={this.handleClickOnSearchButton}
-          style={styles.mainActionButton}
-          className="musicInfo_mainActionButton"
-          variant="extendedFab"
-          size="medium"
-        >
-          <div className="musicInfo_mainActionButton_title">Roll the Wheel</div>
-        </Button>
-      </div>
-    );
-  }
+  return (
+    <div className="musicInfo_mainActionButton_wrapper">
+      <Button
+        onClick={() => props.handleMainSearch()}
+        style={styles.mainActionButton}
+        className="musicInfo_mainActionButton"
+        variant="extendedFab"
+        size="medium"
+      >
+        <div className="musicInfo_mainActionButton_title">Roll the Wheel</div>
+      </Button>
+    </div>
+  );
 }
 
 export default MainActionButton;
