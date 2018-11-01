37b422fbe179c7678ef0999c09c8843cf405fe3a
Thomas <thomas.sypniewski@gmail.com>
2018-11-01 23:35:24 +0100

    Updates

---
+++ precache-manifest.9658fee6bb272b6ba1cce5feeab13b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6e595b0eeb38ff4a951",
+    "url": "/static/js/main.d6e595b0.chunk.js"
+  },
+  {
+    "revision": "f63fc65b80245ee0035c",
+    "url": "/static/js/1.f63fc65b.chunk.js"
+  },
+  {
+    "revision": "d6e595b0eeb38ff4a951",
+    "url": "/static/css/main.dffc7a6f.chunk.css"
+  },
+  {
+    "revision": "4cccacaa9f05ae7b765aaa940d1f2e4a",
+    "url": "/index.html"
+  }
+];
